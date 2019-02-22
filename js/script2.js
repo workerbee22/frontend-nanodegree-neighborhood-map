5acbabaf64751d287899a4c47ce5dff95163fb5d
workerbee22 <mikehughesemail@gmail.com>
2019-02-22 20:50:19 +1100

    Fixed by adding use of Knockout for filtered list of locations.

--- js/script2.js
+++ js/script2.js
@@ -12,18 +12,24 @@ var placeMarkers = [];
 
 // Washington DC momumnents
 var locations = [
-  {title: 'Lincoln Memorial', location: {lat: 38.8893, lng: -77.0502}, id: 0},
-  {title: 'Washington Monument', location: {lat: 38.9072, lng: -77.0369}, id: 1},
-  {title: 'The White House', location: {lat: 38.8977, lng: -77.0365}, id: 2},
-  {title: 'Thomas Jefferson Memorial', location: {lat: 38.8814, lng: -77.0365}, id: 3},
-  {title: 'Smithsonian National Museum of Natural History', location: {lat: 38.8913, lng: -77.0261}, id: 4}
+  {title: 'Lincoln Memorial', lat: 38.8893, lng: -77.0502, id: 0},
+  {title: 'Washington Monument', lat: 38.9072, lng: -77.0369, id: 1},
+  {title: 'The White House', lat: 38.8977, lng: -77.0365, id: 2},
+  {title: 'Thomas Jefferson Memorial', lat: 38.8814, lng: -77.0365, id: 3},
+  {title: 'Smithsonian National Museum of Natural History', lat: 38.8913, lng: -77.0261, id: 4}
 ];
+
+var Locations = function(data) {
+  this.title = ko.observable(data.title);
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
+  this.id = ko.observable(data.id);
+};
 
 function mapsErorr() {
   // Handle a Google Map error
   alert("Google Maps has failed to initialize. Is your internet connection working?");
 }
-
 
 // Google map styling
 function initMap() {
@@ -39,18 +45,18 @@ function initMap() {
   var largeInfowindow = new google.maps.InfoWindow();
 
   // We add a DOM event here to show info window if list item clicked
-  var ul, li, a;
-  ul = document.getElementById("myUL");
-  // console.log(ul);
-  li = ul.getElementsByTagName('li');
-  // Loop through all locations array, hiding those that don't match the search query in list and on map
-  // remove locations.length ... replace with 5 for now
-  for (i = 0; i < li.length; i++) {
-    google.maps.event.addDomListener(li[i], 'click', function() {
-      //window.alert('Item was clicked!');
-      //************ largeInfowindow.open(map, markers[i]);
-    });
-  }
+  // var ul, li, a;
+  // ul = document.getElementById("myUL");
+  // // console.log(ul);
+  // li = ul.getElementsByTagName('li');
+  // // Loop through all locations array, hiding those that don't match the search query in list and on map
+  // // remove locations.length ... replace with 5 for now
+  // for (i = 0; i < li.length; i++) {
+  //   google.maps.event.addDomListener(li[i], 'click', function() {
+  //     //window.alert('Item was clicked!');
+  //     //************ largeInfowindow.open(map, markers[i]);
+  //   });
+  // }
 
   // Style the markers a bit. This will be our listing marker icon.
   var defaultIcon = makeMarkerIcon('0091ff');
@@ -63,7 +69,10 @@ function initMap() {
   // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
-    var position = locations[i].location;
+    // var position = locations[i].location;
+    var lat = locations[i].lat;
+    var lng = locations[i].lng;
+    var position = new google.maps.LatLng(lat, lng);
     var title = locations[i].title;
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
@@ -139,7 +148,7 @@ function populateInfoWindow(marker, infowindow) {
     // 50 meters of the markers position
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
     // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
+    // infowindow.open(map, marker);
   }
 }
 
@@ -169,75 +178,61 @@ function makeMarkerIcon(markerColor) {
 } // markerIcon END
 
 // Location object
-var Location = function(data) {
-  // Data created for name from locations array and the title property
-  this.name = ko.observable(data.title);
-  this.id = ko.observable(data.id);
-};
+// var Location = function(data) {
+//   // Data created for name from locations array and the title property
+//   this.name = ko.observable(data.title);
+//   this.id = ko.observable(data.id);
+// };
 
 // ViewModel - because it has logic to increase the click count when image clicked
-var searchBar = function () {
-  
-  // Declare variables
-    var input, filter, ul, li, a, i;
+// var searchBar = function () {
+//
+//   // Declare variables
+//     var input, filter, ul, li, a, i;
+//
+//     input = document.getElementById('places-search');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+//
+//     // Loop through all locations array, hiding those that don't match the search query in list and on map
+//     // remove locations.length ... replace with 5 for now
+//     for (i = 0; i < li.length; i++) {
+//       // a = li[i].getElementsByTagName("a")[0];
+//       if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//         // Show map marker
+//         markers[i].setMap(map);
+//       } else {
+//         li[i].style.display = "none";
+//         // Hide map marker
+//         markers[i].setMap(null);
+//       }
+//     }
+// }
 
-    input = document.getElementById('places-search');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all locations array, hiding those that don't match the search query in list and on map
-    // remove locations.length ... replace with 5 for now
-    for (i = 0; i < li.length; i++) {
-      // a = li[i].getElementsByTagName("a")[0];
-      if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-        // Show map marker
-        markers[i].setMap(map);
-      } else {
-        li[i].style.display = "none";
-        // Hide map marker
-        markers[i].setMap(null);
-      }
-    }
-}
-
-var stuff = function () {
+var viewModel = function () {
   
   // for use with the SELF trick below in incrementCounter
   var self = this;
   
-  // catList ko observable array created, then populated with new Location objects from locations array
-  this.locationList = ko.observableArray([]);
-  // Using forEach for the locations array. Note: The forEach() method executes a provided function once for each array element.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-  // Each location object data in locations array, passed to locationitem on the anonymous function
-  // that anonymous function then pushes a new location object to the locationList ko observable array
-  locations.forEach(function (locationItem) {
-    self.locationList.push(new Location(locationItem));
+  self.placeList = ko.observableArray([]);
+  self.query = ko.observable('');
+
+  locations.forEach(function(placeLocation) {
+    self.placeList.push(new Locations(placeLocation));
   });
-  // console.dir(this.locationList);
-  
-  // currentLocation set to a Location object. Use just the first location Object in the locations array
-  this.currentLocation = ko.observable(new Location(locations[0]));
-  
-  // this.incrementCounter = function () {
-  //   // becuase a ko observable, need to get its value differently by calling it as a function ie. this.clickCount()
-  //   // and set its value by passing in the new value ie. this.clickCount( ... )
-  //   // SELF trick to keep outter function THIS
-  //   self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-  // };
-  
-  this.setCurrentLocation = function (loc) {
-    // console.log('***click');
-    // console.log(loc);
-    // change currentLocation which holds just 1 location object location that was clicked.
-    self.currentLocation(loc);
-    // Open infowindow of corresponding marker
-    //initMap.largeInfowindow.open(map, markers.loc.id);
-    
-  };
+
+  // everytime query/placeList changes, this gets computed again
+  self.filteredPlaces = ko.computed(function() {
+    if (!self.query()) {
+      return self.placeList();
+    } else {
+      return self.placeList()
+        .filter(place => place.title().toLowerCase().indexOf(self.query().toLowerCase()) > -1);
+    }
+  });
   
 };
 
-ko.applyBindings(new stuff());
+ko.applyBindings(new viewModel());
